e20a7e6bd7d62c1f1c6a99908cbacc547ac3029c
Carloluis Rodriguez <carloluisr@gmail.com>
2017-07-20 22:25:28 -0300

    feature/reduce-slug-size (#3)
    * check for client/ and server/ dependencies
    
    * fix typo in compile script
    
    * check subdirectories for existing sub-projects
    
    * compile script fix (use `[[` instead of `[`)
    
    * update dependencies script
    * value expansions should be double-quoted
    
    * compile - check for subprojects when restoring dependencies

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -9,10 +9,10 @@ yarn_node_modules() {
 
 npm_node_modules() {
 	local build_dir=${1:-}
-	if [ -e $build_dir/package.json ]; then
+	if [ -e "$build_dir"/package.json ]; then
 		echo "install dependencies with npm"
-		cd $build_dir
-		npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+		cd "$build_dir"
+		npm install --unsafe-perm --userconfig "$build_dir"/.npmrc 2>&1
 	else
 		echo "Skipping (no package.json)"
 	fi
