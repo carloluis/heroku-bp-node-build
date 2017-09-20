71eb36762dc0c53b2abf9b72849dd0ce3421b768
Carloluis Rodriguez <carloluisr@gmail.com>
2017-09-20 17:58:03 -0300

    update/environment vars (#5)
    * remove regex for whitelisting vars
    
    * remove indent (non interactive stream editor) from export_env_dir outputs
    
    * indent echo vars in environment script

--- lib/environment.sh
+++ lib/environment.sh
@@ -2,13 +2,12 @@
 
 function export_env_dir() {
 	local env_dir="$1"
-	local whitelist_regex="${2:-''}"
-	local blacklist_regex="${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LANG)$'}"
+	local blacklist_regex="${2:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LANG)$'}"
 
 	if [[ -d "$env_dir" ]]; then
 		for e in $(ls "$env_dir"); do
-			echo "$e=$(cat $env_dir/$e)"
-			echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" && export "$e=$(cat $env_dir/$e)"
+			echo "       $e=$(cat $env_dir/$e)"
+			echo "$e" | grep -qvE "$blacklist_regex" && export "$e=$(cat $env_dir/$e)"
 			:
 		done
 	fi
